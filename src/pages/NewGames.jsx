cc806a5381bd1654abe55fef3674528d6d34bd00
Michał Leszczyk <michasko37@gmail.com>
2023-07-31 13:59:07 +0200

    proxy do moby games

--- src/pages/NewGames.jsx
+++ src/pages/NewGames.jsx
@@ -3,26 +3,18 @@ import React, { useState, useEffect } from "react";
 export default function NewGames() {
   const [list, setList] = useState(null);
 
-  // useEffect(() => {
-  //   fetch("/moby/")
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       }
-  //     })
-  //     .then((data) => {
-  //       setList(data);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch("/moby/games?format=brief")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log('Lista gierek', data);
+        setList(data);
+      });
+  }, []);
+
   return <div>NewGames</div>;
 }
-
-
-// fetch("/moby/", {
-//   method: "POST",
-//   Body: "fields *;",
-//   headers: {
-//     "Client-ID": "fpqvqyvv64hh2f2gp08e5grjr5nt94",
-//     "Authorization": "Bearer wf6ymq3e31joxamg67awjt6wz9rul7",
-//   },
-// })
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,46 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const MOBY_GAMES_BASE_URL = "https://api.mobygames.com/v1"
 
-  server:{
-    proxy:{
-      "/moby/":" https://api.mobygames.com/v1/games?api_key=moby_NDAotuC9wPRKn4wK1sflQnViLYc"
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+
+    server:{
+      // Ustawiamy serwer proxy na Vite
+      proxy:{
+        // Dla kazdego zadania, ktore przyjdzie pod sciezke /moby
+        "/moby": {
+          // Przekierowujemy je do serwera docelowego (API Moby Games):
+          target: MOBY_GAMES_BASE_URL,
+          // Usuwamy "/moby" ze sciezki
+          rewrite: (path) => path.replace(/^\/moby/, ''),
+          secure: false,
+          changeOrigin: true,
+          configure: (proxy, _options) => {
+            // Dodajemy klucze API do kazdego zadania, ktore wychodzi do serwera
+            // zebysmy nie musieli ich dorzucac bezposrednio z frontendu
+            proxy.on('proxyReq', (proxyRequest, apiRequest, apiResponse) => {
+              
+              // Tworze obiekt URL - pomoze mi to rozdzielic adres do API
+              // na jego poszczegolne czesci: domenę, ściezke i parametry
+              // (tzw. query string)
+              const url = new URL(proxyRequest.path, MOBY_GAMES_BASE_URL);
+              
+              // Do query string dodaję parametr api_key
+              url.searchParams.append('api_key', encodeURIComponent(env.VITE_APP_MOBY_GAMES_API_KEY));
+              
+              // Na koniec podmieniam sciezke w requescie, ktory Vite wyśle do API Moby Games,
+              // tak aby zawierala ona klucz API
+              proxyRequest.path = url.pathname + url.search;
+            })
+          }
+        },
+      }
     }
   }
-})
+});
