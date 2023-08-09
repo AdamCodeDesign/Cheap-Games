b98ab6987fe8de90839992b1d8837801c6bacb54
Adam <adam@MacBook-Pro-Adam.local>
2023-08-09 15:50:26 +0200

    Newgames maja yeraz gry z tego roku

--- src/pages/NewGames.jsx
+++ src/pages/NewGames.jsx
@@ -1,47 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Browser from "../components/Browser";
-import GameList from "../components/GameList";
 
 export default function NewGames() {
-  // const [list, setList] = useState([]);
-  // const [error, setError] = useState("");
-
-  // useEffect(() => {
-  //   fetch("/moby/games?")
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       }
-  //       return {
-  //         error: "Loading games...",
-  //       };
-  //     })
-  //     .then((data) => {
-  //       if (data.error) {
-  //         setList([]);
-  //         setError(data.error);
-  //       } else {
-  //         setError("");
-  //         console.log("Lista wszystkich gierek", data);
-  //         const filteredGames = data.games.filter((game) => {
-  //           return game.platforms.some(
-  //             (platform) => parseInt(platform.first_release_date) > 2010
-  //           );
-  //         });
-  //         // console.log({filteredGames})
-  //         setList(filteredGames);
-  //         console.log("przefiltrowane", filteredGames);
-  //       }
-  //     });
-  // }, []);
-
-  // console.log("to jest objekt", list)
-
-
+ 
   return (
     <>
-      <Browser />
-      {/* <GameList/> */}
+      <Browser filter = "dates=2023-01-01,2023-08-09"/>
     </>
   );
 }
