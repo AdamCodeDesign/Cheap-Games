f26a093e6a4afac85fb9e59f62a22d3807f4247f
Adam <adam@MacBook-Pro-Adam.local>
2023-10-26 22:10:08 +0200

    test

--- src/pages/NewGames.jsx
+++ src/pages/NewGames.jsx
@@ -5,7 +5,7 @@ export default function NewGames() {
   return (
     <>
     <h1 style={{fontSize:"3rem", color: "#37BFC9", marginTop:"100px", marginBottom:"30px"}}>Games of 2023 year</h1>
-      <Genres platform="?&dates=2023-01-01,2023-08-09" sale={1}/>
+      <Genres platform="?&dates=2023-01-01,2023-10-19" sale={1}/>
     </>
   );
 }
