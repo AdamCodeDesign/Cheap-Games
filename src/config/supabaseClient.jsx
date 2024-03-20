220f1b72f62fb9066f2f9eb219c2c00a5f7c3805
LeshchuDev <leshchudev@gmail.com>
2024-03-20 19:35:42 +0100

    test supabaseUrl

--- src/config/supabaseClient.jsx
+++ src/config/supabaseClient.jsx
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
+const supabaseAdres = import.meta.env.VITE_APP_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_APP_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseAdres, supabaseKey);
 
 export default supabase;
