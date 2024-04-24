b8791450f37fa0fdeb974a65ec077b764d836e90
LeshchuDev <leshchudev@gmail.com>
2024-04-24 22:20:51 +0200

    Shop rules changed

--- src/pages/ShopRules.jsx
+++ src/pages/ShopRules.jsx
@@ -2,111 +2,23 @@ import React from "react";
 
 export default function ShopRules() {
   return (
-    <div style={{display:"flex", flexDirection:"column", gap:30}}>
+    <div
+      style={{
+        height: "calc(100vh - 130px)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "30px",
+      }}
+    >
       <h1>ShopRules</h1>
-      <h2>TERMS OF SERVICE ACCEPTANCE OF THE TERMS OF SERVICE</h2>
-
       <p>
-        THE FOLLOWING TERMS AND CONDITIONS (“TERMS”) APPLY TO YOUR USE OF THE
-        WEBSITES OF EPIC GAMES, INC. AND ITS AFFILIATES (“EPIC”, “WE” “US” OR
-        “OUR”), INCLUDING ANY CONTENT, FUNCTIONALITY, PRODUCTS, AND SERVICES
-        OFFERED ON OR THROUGH SUCH WEBSITES (COLLECTIVELY, THE “WEBSITE”),
-        WHETHER AS A GUEST OR A REGISTERED USER. THESE TERMS ALSO APPLY TO YOUR
-        USE OF OTHER EPIC SERVICES THAT DISPLAY OR INCLUDE THESE TERMS
-        (“ADDITIONAL SERVICES”). IN THESE TERMS, THE WEBSITE AND ADDITIONAL
-        SERVICES ARE COLLECTIVELY REFERRED TO AS THE “SERVICES.”
-      </p>
-
-      <p>
-        PLEASE READ THESE TERMS CAREFULLY BEFORE YOU START TO USE THE SERVICES.
-        BY USING THE SERVICES, YOU ACCEPT AND AGREE TO BE BOUND AND ABIDE BY
-        THESE TERMS OF SERVICE. IF YOU DO NOT WANT TO AGREE TO THESE TERMS OF
-        SERVICE, YOU MUST NOT USE THE SERVICES. IN PARTICULAR, WE WANT TO
-        HIGHLIGHT SOME IMPORTANT TERMS, POLICIES, AND PROCEDURES IN THESE TERMS.
-        BY ACCEPTING THESE TERMS:
-      </p>
-
-      <p>
-        You are also agreeing to other Epic rules and policies that are
-        expressly incorporated into and a part of these Terms. Please read them
-        carefully: Our Privacy Policy explains what information we collect from
-        you and how we protect it. Our Fan Content Policy explains what you can
-        do with Epic’s Intellectual Property in the content you create. You and
-        Epic agree to resolve disputes between us in individual arbitration (not
-        in court). We believe the alternative dispute-resolution process of
-        arbitration will resolve any dispute fairly and more quickly and
-        efficiently than formal court litigation. We explain the process in
-        detail below, but we’ve put this up front (and in caps) because it’s
-        important:
-      </p>
-
-      <p>
-        THESE TERMS CONTAINS A BINDING, INDIVIDUAL ARBITRATION AND CLASS-ACTION
-        WAIVER PROVISION. IF YOU ACCEPT THESE TERMS, YOU AND EPIC AGREE TO
-        RESOLVE DISPUTES IN BINDING, INDIVIDUAL ARBITRATION AND GIVE UP THE
-        RIGHT TO GO TO COURT INDIVIDUALLY OR AS PART OF A CLASS ACTION, AND EPIC
-        AGREES TO PAY YOUR ARBITRATION COSTS FOR ALL DISPUTES OF UP TO $10,000
-        THAT ARE MADE IN GOOD FAITH (SEE BELOW).
-      </p>
-
-      <p>
-        TO ENTER INTO THE CONTRACT CREATED BY THESE TERMS, YOU MUST BE AN ADULT
-        OF THE LEGAL AGE OF MAJORITY IN YOUR COUNTRY OF RESIDENCE. YOU ARE
-        LEGALLY AND FINANCIALLY RESPONSIBLE FOR ALL ACTIONS USING OR ACCESSING
-        OUR SOFTWARE, INCLUDING THE ACTIONS OF ANYONE YOU ALLOW TO ACCESS TO
-        YOUR ACCOUNT. YOU AFFIRM THAT YOU HAVE REACHED THE LEGAL AGE OF
-        MAJORITY, UNDERSTAND AND ACCEPT THESE TERMS (INCLUDING ITS DISPUTE
-        RESOLUTION TERMS). IF YOU ARE UNDER THE LEGAL AGE OF MAJORITY, YOUR
-        PARENT OR LEGAL GUARDIAN MUST CONSENT TO THESE TERMS.
-      </p>
-
-      <p>
-        In addition to these Terms, software or services that are included in or
-        otherwise made available to you through the Services may be subject to
-        separate agreement between you and Epic, such as end user license
-        agreements. If these Terms are inconsistent with any such agreements,
-        those agreements will control.
-      </p>
-
-      <h2>Privacy Notice</h2>
-
-      <p>
-        Please review our Privacy Notice found at
-        https://www.epicgames.com/privacypolicy, which also governs your use of
-        the Services, to understand our practices.
-      </p>
-
-      <h2>Changes to the Terms of Service</h2>
-
-      <p>
-        We may update these Terms from time to time; you should check this page
-        regularly to take notice of any changes. Your continued use of the
-        Services following the posting of revised Terms means that you accept
-        and agree to the changes.
-      </p>
-
-      <h2>Accessing the Services and Account Security</h2>
-
-      <p>
-        We may withdraw or amend the Services, and any related service or
-        content, or restrict access (including by means of cancellation,
-        termination, or modification, or suspension of a user account) to all or
-        certain users (including you) without notice and without liability to
-        you in our reasonable discretion. Additionally, due to your geographic
-        location, the Services or some of their features, services, or content
-        may be unavailable to you. Notwithstanding anything to the contrary
-        herein, we may terminate or suspend access to the Services based on your
-        breach of these Terms.
-      </p>
-
-      <p>
-        To access certain Services, you will be asked to provide registration
-        details or other information, and in order to use such resources, all
-        the information you provide must be correct, current, and complete. From
-        time to time, in order to access the Services or certain games,
-        services, or functionality, Epic may require some or all users to
-        download updated or additional software. The terms of use of such
-        software may be subject to separate agreement between you and Epic.
+        This is only a illustrative store interface. The store doesn't really
+        exist. This is a project for learning programming. All data is not
+        processed and made available to third parties. If you have created an
+        account here and want to delete it, please contact me. Contact details
+        in the 'contact' tab below.
       </p>
     </div>
   );
