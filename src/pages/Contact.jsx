23d00bbb4c3d872d6fe06e260180a94c9dbeae0e
LeshchuDev <leshchudev@gmail.com>
2025-03-02 13:42:11 +0100

    contact bug fixed, email centered

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,7 +14,7 @@ export default function Contact() {
                 <article className="adress col-6">
                     <h1>Sklep Cheap Games</h1>
                 </article>
-                <article className="contacts col-6">
+                <article className="contacts col-12">
                     <h1>Phone number</h1>
                     <h1>669-848-852</h1>
                     <h1>e-mail: adamcodedesign@gmail.com</h1>
